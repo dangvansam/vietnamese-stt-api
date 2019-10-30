7a240e0f28a97ee59781cbe3ed044fc311fc3a15
Sâm <45785880+dangvansam98@users.noreply.github.com>
2019-10-30 12:11:48 +0700

    try except

--- request-stt-vietel-fpt.py
+++ request-stt-vietel-fpt.py
@@ -3,45 +3,78 @@
 import requests
 import json
 import os
+from lxml.html import fromstring
 
-def requestVTC(filename):
+def get_proxies():
+    url = 'https://free-proxy-list.net/'
+    response = requests.get(url)
+    parser = fromstring(response.text)
+    proxies = set()
+    for i in parser.xpath('//tbody/tr')[:10]:
+        if i.xpath('.//td[7][contains(text(),"yes")]'):
+            #Grabbing IP and corresponding PORT
+            proxy = ":".join([i.xpath('.//td[1]/text()')[0], i.xpath('.//td[2]/text()')[0]])
+            proxies.add(proxy)
+    return proxies
+
+def requestVTC(filename, proxy): 
     url = "https://vtcc.ai/voice/api/asr/v1/rest/decode_file"
     headers = {
-        'token': 'anonymous',
+        'token': 'ulTkA-jNoKXB78VqO1Qg5GQ5eIeO91dlpi7n9WdnhvhADz6T0wzFxrw8iBkOCD4R',
         'sample_rate': '16000',
         #'format':'S16LE',
         'num_of_channels':'1',
         #'asr_model': 'model code'
         }
+    #qpupDIOO3HY6HATdGde9ve0VzXMZl-SjSrT9RVv-Vz9nF3VsJtguOrpJzGzBad1o, anonymous
+    
     s = requests.Session()
     files = {'file': open(filename,'rb')}
-    response = requests.post(url,files=files, headers=headers, verify='wwwvtccai.crt')
-
-    res_json = response.json()
-    if res_json[0]['status'] == 0:
-        transcript = res_json[0]['result']['hypotheses'][0]['transcript_normed']
-        #print('request VTC success')
-        print('VTC transcript:',transcript)
-        return transcript
+    if proxy != None:
+        response = requests.post(url,files=files, headers=headers, verify='wwwvtccai.crt', proxy = proxy)
     else:
+        response = requests.post(url,files=files, headers=headers, verify='wwwvtccai.crt')
+    #print(response.text)
+    res_json = response.json()
+    #print(res_json)
+    try:
+        a = res_json[0]['status']
+        if a == 0:
+            transcript = res_json[0]['result']['hypotheses'][0]['transcript_normed']
+            #print('request VTC success')
+            print('VTC transcript:',transcript)
+            if len(transcript) == 0:
+                return None
+            else:
+                return transcript
+    except:
         return None
 
 def requestFPT(filename):
     url = 'https://api.fpt.ai/hmi/asr/general'
     payload = open(filename, 'rb').read()
-    headers = {'api-key': 'sign in (https://console.fpt.ai/), enable STT api and paste your token here'} #examle: 'api-key': '3ISvE45DVemWTvrMTIgMtyfIjHnd8yAz'
-    response = requests.post(url=url, data=payload, headers=headers)
-    res_json = response.json()
-    #print(res_json)
-    if res_json['status'] == 0:
-        transcript = res_json['hypotheses'][0]['utterance']
-        #print('request FPT success')
-        print('FPT transcript:',transcript)
-        return transcript
-    else:
+    headers = {'api-key': 'eqJwxYhIkfgkCS1LB8ON5d7jKfEgj2na'} #examle: 'api-key': '3ISvE45DVemWTvrMTIgMtyfIjHnd8yAz' , qpupDIOO3HY6HATdGde9ve0VzXMZl-SjSrT9RVv-Vz9nF3VsJtguOrpJzGzBad1o
+    try:
+        response = requests.post(url=url, data=payload, headers=headers)
+        print(response)
+        res_json = response.json()
+        print(res_json)
+        a = res_json['status']
+        if a == 0:
+            transcript = res_json['hypotheses'][0]['utterance']
+            #print('request FPT success')
+            print('FPT transcript:',transcript)
+            if len(transcript) == 0:
+                return None
+            else:
+                return transcript
+        else:
+            return None
+    except:
         return None
-
-def requestAndWriteFile(audio_dir_path = 'cuted', transcript_out_dir = 'cuted_transcript'):
+        
+from time import sleep
+def requestAndWriteFile(audio_dir_path, transcript_out_dir):
     audio_dir = audio_dir_path + '/'
     if not os.path.exists(audio_dir):
         print('Audio dir: "{}" not found! Plase check input dir'.format(audio_dir_path))
@@ -51,23 +84,40 @@ def requestAndWriteFile(audio_dir_path = 'cuted', transcript_out_dir = 'cuted_tr
         os.mkdir(transcript_dir)
     print('Input audio files dir: {}'.format(audio_dir))
     print('Output transcript files dir: {}'.format(transcript_dir))
+    proxies = {'118.174.232.237:48665', '202.29.237.213:3128', '101.109.243.99:8080'}#get_proxies()
     for f in os.listdir(audio_dir):
-        name_label_file = transcript_dir + f.split('.')[0]+ '.txt'
+        name_label_file = transcript_dir + f.split('/')[-1].split('.')[0]+ '.txt'
         audio_path = audio_dir + f
         if os.path.isfile(name_label_file):
             print('Transript for: {} is exist in: {}. Skipped'.format(audio_path,name_label_file))
             continue
         else:
-            label_file = open(name_label_file, 'w', encoding='utf-8')
-            res = requestFPT(audio_path)
+            #sleep(3)
+            #res = requestFPT(audio_path)
+            # if res == None:
+            #     sleep(3)
+            res = requestVTC(audio_path, None)
+            sleep(1)
+            # for ip in proxies:
+            #     sleep(2)
+            #     try:
+            #         print('ip:',ip)
+            #         ip = {"http": ip, "https": ip}
+            #         res = requestVTC(audio_path,ip)
+            #         print(res)
+            #     except:
+            #         res = None
+            #     if res != None:
+            #         print('success ip:',ip)
+            #         break
             if res == None:
-                print('request vtc api failed. trying fpt api')
-                res = requestVTC(audio_path)
-                if res == None:
-                    print('all api failed! skip')
-                    exit()
-            label_file.write(res)
-            print('Transript success, file:{}'.format(name_label_file))
+                continue
+            else:
+                label_file = open(name_label_file, 'w', encoding='utf-8')
+                label_file.write(res)
+                print('Transript success, file:{}'.format(name_label_file))
+                sleep(2)
         #break
-
-requestAndWriteFile('cuted', 'cuted_transcript')
+#print(get_proxies())
+requestAndWriteFile('H:/ASR/dataset ASR/5000 file gốc/all_wav_thai_son', 'all_wav_thai_son_transcript')
+#requestAndWriteFile('to_tieng_cuted', 'to_tieng_transcript')
